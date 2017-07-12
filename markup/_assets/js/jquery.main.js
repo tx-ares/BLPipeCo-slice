d760560cf87d6153dd90e848dab25031e546d61a
txsadamwest <twinadanz@gmail.com>
2017-07-12 14:23:27 -0500

    style fixes

--- markup/_assets/js/jquery.main.js
+++ markup/_assets/js/jquery.main.js
@@ -552,7 +552,7 @@ function initPanelSlider(){
 		    {
 		      breakpoint: 1024,
 		      settings: {
-		        slidesToShow: 3,
+		        slidesToShow: 4,
 		        slidesToScroll: 1,
 	      		infinite: true
 		      }
